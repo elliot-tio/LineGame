63bc3294f47a86e12137644949b3aba8d18e2139
Elliot Tio <etjinzo@gmail.com>
2021-04-12 00:36:56 -0700

    boxes filling - still some bugs, and missing multi-box completion

--- script.js
+++ script.js
@@ -2,12 +2,17 @@ const canvas = document.getElementById('canvas');
 const leftEdge = canvas.offsetLeft + canvas.clientLeft;
 const topEdge = canvas.offsetTop + canvas.clientTop;
 const context = canvas.getContext('2d');
-const lines = [];
+let lines = drawn = horizontalBoxLines = verticalBoxLines = [];
+let boxes = {
+    "Player 1": [],
+    "Player 2": []
+};
 const buffer = 5;
+let winCond = 0;
 
 const player1 = {
     name: 'Player 1',
-    color: 'blue'
+    color: 'aqua'
 }
 
 const player2 = {
@@ -17,61 +22,142 @@ const player2 = {
 
 let currentPlayer = player1 ? player1 : player2;
 
-canvas.addEventListener('click', function(event) {
+canvas.addEventListener('click', takeTurn, false);
+
+// click event that draws edges for a player, triggered when a player clicks on an edge
+function takeTurn(event) {
     const x = event.pageX - leftEdge;
     const y = event.pageY - topEdge;
 
     // Collision detection between clicked offset and element.
-    lines.forEach(function(line) {
+    lines.forEach((line, index) => {
         if (y > line.top && y < line.top + line.height 
             && x > line.left && x < line.left + line.width) {
-            console.log('clicked an element');
-            console.log(currentPlayer)
+
+            // if line was already drawn, don't draw it again
+            if(drawn.includes(line)) return;
+            // add line to drawn array so that the line won't be drawn over again
+            drawn.push(line);
+
             context.fillStyle = currentPlayer.color;
+            context.strokeStyle = 'black';
+
             if(line.type === 'horizontal') {
+                // draw horizontal line
                 context.fillRect(line.left, line.top + buffer, line.width, line.height - (2 * buffer));
+                context.strokeRect(line.left, line.top + buffer + 1, line.width, line.height - (2 * buffer) - 2);
+                // filter out lines that could possibly make a box with drawn line
+                horizontalBoxLines = drawn.filter(hLine => hLine.type === 'horizontal' && hLine.x === line.x && Math.abs(hLine.y - line.y) <= 1);
             } else if (line.type === 'vertical') {
+                // draw vertical line
                 context.fillRect(line.left + buffer, line.top, line.width - (2 * buffer), line.height);
+                context.strokeRect(line.left + buffer + 1, line.top, line.width - (2 * buffer) - 2, line.height);
+                // filter out lines that could possibly make a box with drawn line
+                verticalBoxLines = drawn.filter(vLine => vLine.type === 'vertical' && Math.abs(vLine.x - line.x) <= 1 && vLine.y === line.y);
             }
-            currentPlayer = currentPlayer === player1 ? player2 : player1;
-            // todo: if clicked, camt click again
-            // todo: if completed box, color in box of player
-            // todo: if completed box, player goes again
+
+            // todo: if completed box, color in box of player - missing multiple box completion, there are bugs with the box creation
+            let completedBox = false;
+            // we've found at least 4 edges that make a box - fill in the box with the color of the player that completed it
+            if(horizontalBoxLines.length + verticalBoxLines.length >= 4) {
+                console.log(horizontalBoxLines, verticalBoxLines)
+                let boxHeight = boxWidth = boxLeft = boxTop = 0;
+                if(horizontalBoxLines.length >= 2) {
+                    // more than one box completed
+                }
+                if(verticalBoxLines.length >= 2) {
+                    // more than one box completed
+                }
+
+                // one box completed
+                if(line.type === 'horizontal') {
+                    boxHeight = verticalBoxLines[0].height;
+                    boxWidth = line.width;
+                    // get minimum values of left/top from vertical lines so we know where to fill the box from
+                    boxLeft = verticalBoxLines.reduce((prev, curr) => prev.left < curr.left ? prev.left : curr.left);
+                    boxTop = verticalBoxLines.reduce((prev, curr) => prev.top < curr.top ? prev.top : curr.top );
+                    // todo: check if box was already filled
+                    context.fillRect(boxLeft + buffer + 5, boxTop, boxWidth, boxHeight);
+                    // push values to a boxes array under the player name, to check for win condition
+                    boxes[currentPlayer.name].push({
+                        left: boxLeft + buffer + 5,
+                        top: boxTop,
+                        width: boxWidth,
+                        height: boxHeight
+                    })
+                } else if (line.type === 'vertical') {
+                    boxHeight = line.height;
+                    boxWidth = horizontalBoxLines[0].width;
+                    // get minimum values of left/top from horizontal lines so we know where to fill the box from
+                    boxLeft = horizontalBoxLines.reduce((prev, curr) => prev.left < curr.left ? prev.left : curr.left);
+                    boxTop = horizontalBoxLines.reduce((prev, curr) => prev.top < curr.top ? prev.top : curr.top );
+                    // todo: check if box was already filled
+                    context.fillRect(boxLeft, boxTop + buffer + 5, boxWidth, boxHeight);
+                    // push values to a boxes array under the player name, to check for win condition
+                    boxes[currentPlayer.name].push({
+                        left: boxLeft,
+                        top: boxTop + buffer + 5,
+                        width: boxWidth,
+                        height: boxHeight
+                    })
+                }
+                completedBox = true;
+            }
+
+
+            // when completing a box, check if the win condition has been met
+            if(completedBox) {
+                console.log('completed box!')
+                if(boxes[currentPlayer.name].length >= winCond) {
+                    context.fillStyle = 'red';
+                    context.font = '50px verdana';
+                    context.fillText(`${currentPlayer.name} has Won!`, 95, 55)
+                    canvas.removeEventListener('click', takeTurn);
+                }
+            } else {
+                // swap player if box not completed
+                currentPlayer = currentPlayer === player1 ? player2 : player1;
+            }
         }
     });
-}, false);
+}
 
 // draw grid, start game
 function drawGrid() {
     clearCanvas();
     context.fillStyle = 'black'
-    let size = parseInt(document.getElementById('gridSize').value) + 1;
-    context.strokeStyle = '#f0f0f0';
+    let size = parseInt(document.getElementById('gridSize').value);
+    // win condition - minimum number of boxes required is 1 more than half the grid size
+    winCond = Math.floor(((size - 1) * (size - 1)) / 2 + 1);
+    size += 1;
 
     // horizontal lines
     for (x = 1; x < size - 1; x++) {
         for (y = 1; y < size; y++) {
-            context.strokeRect(canvas.width / size * x + 5, canvas.height / size * y - buffer, canvas.width / size - 5, 5 + (2 * buffer));
             lines.push({
                 left: canvas.width / size * x + 5,
                 top: canvas.height / size * y - buffer,
                 width: canvas.width / size - 5,
                 height: 5 + (2 * buffer),
-                type: 'horizontal'
+                type: 'horizontal',
+                x: x,
+                y: y
             })
         }
     }
+
     
     // vertical lines
     for (x = 1; x < size; x++) {
         for (y = 1; y < size - 1; y++) {
-            context.strokeRect(canvas.width / size * x - buffer, canvas.height / size * y + 5, 5 + (2 * buffer), canvas.height / size - 5);
             lines.push({
                 left: canvas.width / size * x - buffer,
                 top: canvas.height / size * y + 5,
                 width: 5 + (2 * buffer),
                 height: canvas.height / size - 5,
-                type: 'vertical'
+                type: 'vertical',
+                x: x,
+                y: y
             })
         }
     }
@@ -82,12 +168,19 @@ function drawGrid() {
             context.fillRect(canvas.width / size * x, canvas.height / size * y, 5, 5);
         }
     }
-
 }
 
 // clear canvas
 function clearCanvas() {
     context.clearRect(0, 0, canvas.width, canvas.height);
+    lines = [];
+    drawn = [];
+    horizontalBoxLines = [];
+    verticalBoxLines = [];
+    boxes = {
+        "Player 1": [],
+        "Player 2": []
+    };
 }
 
 drawGrid()
